68bcb286a1898d90681a676d21b1c495d17e9384
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2025-02-14 20:28:08 +0000

    update project

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -3,12 +3,9 @@ function switchLangBtn() {
     if (document.documentElement.lang == "en") {
         btn.innerHTML = "in English please"
         switchLangDoc()
-        toggleLangContent()
-
     } else {
         btn.innerHTML = "en français s'il vous plaît"
         switchLangDoc()
-        toggleLangContent()
     }
 }
 
@@ -19,24 +16,3 @@ function switchLangDoc() {
         document.documentElement.lang = "en"
     }
 }
-
-// function below not working
-
-function toggleLangContent() {
-    let englishContent = document.getElementsByClassName("en");
-    let frenchContent = document.getElementsByClassName("fr");
-    if (document.documentElement.lang == "en") {
-        for (let i = 0; i < englishContent.length; i++) {
-            frenchContent[i].style.display = "none"
-            englishContent[i].style.display = "block"
-        }
-    } 
-    if (document.documentElement.lang == "fr") {
-        for (let j = 0; j < frenchContent.length; j++) {
-            frenchContent[j].style.display = "block"
-            englishContent[j].style.display = "none"
-        }
-    }
-
-}
-
